e01dfcc5678a30f62d7a822dd982b46523410cfb
aileks <aeverly14@pm.me>
2024-04-26 16:34:26 -0400

    resetting

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,12 +25,3 @@ callback = function(e)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end
 })
-
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
