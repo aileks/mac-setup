dd1f26eea1c8a445c54713d56fd020afbfe08cca
Aaliyah Harper <aeverly14@pm.me>
2025-08-15 11:29:22 -0400

    add fastfetch to cli tools install

--- install.zsh
+++ install.zsh
@@ -125,7 +125,7 @@ install_homebrew() {
 install_cli_tools() {
     show_progress "Installing command line tools"
 
-    local cli_tools=(git curl wget tmux mise ripgrep fzf trash-cli tree btop jq gh bash eza zoxide)
+    local cli_tools=(git curl wget tmux mise ripgrep fzf trash-cli fastfetch tree btop jq gh bash eza zoxide)
     local failed_installs=()
 
     for tool in "${cli_tools[@]}"; do
