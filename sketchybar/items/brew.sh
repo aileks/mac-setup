7061e4668841aaecd711e0be399620576f9a95b6
Aaliyah Harper <aeverly14@pm.me>
2025-08-24 17:19:58 -0400

    Switch default browser to Brave, update font and brew freq

--- sketchybar/items/brew.sh
+++ sketchybar/items/brew.sh
@@ -5,6 +5,6 @@ source "$CONFIG_DIR/colors.sh"
 PLUGIN_DIR="$CONFIG_DIR/plugins"
 
 sketchybar --add item brew right \
-           --set brew update_freq=28800 \
-           script="$PLUGIN_DIR/brew.sh" \
-           background.color=$BG1
+  --set brew update_freq=7200 \
+  script="$PLUGIN_DIR/brew.sh" \
+  background.color=$BG1
